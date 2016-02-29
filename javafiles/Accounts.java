8b333129cbc63d6f46572fe21820452a04a720cb
Antonio La Rocca <antonio.larocca@mail.utoronto.ca>
2016-02-29 13:50:24 -0500

    Created an accounts class base outline along with all getters and setters so far

--- javafiles/Accounts.java
+++ javafiles/Accounts.java
@@ -1,5 +1,58 @@
 package project;
 
-public class Accounts {
+import java.sql.Date;
 
+public class Accounts {
+  static final int TYPE_STUDENT = 0;
+  static final int TYPE_PROFESSOR = 1;
+  
+  int accountType;
+  String firstName;
+  String lastName;
+  String email;
+  Date birthday;
+  
+  public Accounts(){
+    
+  }
+  
+  public int getAccountType() {
+    return accountType;
+  }
+
+  public void setAccountType(int accountType) {
+    this.accountType = accountType;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(Date birthday) {
+    this.birthday = birthday;
+  }
 }
