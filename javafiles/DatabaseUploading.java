c6219d6089ab4ebd256fc64482bd2fc793c896ec
Ertan A <ertan.aksoy@mail.utoronto.ca>
2016-03-04 18:28:45 -0500

    PROJ3-10 finished skeleton code for database classes

---
+++ javafiles/DatabaseUploading.java
@@ -0,0 +1,25 @@
+public class DatabaseUploading
+{
+
+	public static void addDocument()
+	{
+
+	}
+
+
+	public static void addUser()
+	{
+
+	}
+
+	public static void likeADocument()
+	{
+
+	}
+
+	public static void commentOnADocument()
+	{
+		
+	}
+
+}
