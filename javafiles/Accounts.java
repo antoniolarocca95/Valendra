bad4870ce24da85f7f2f2b8237ce784fd53888aa
Antonio La Rocca <antonio.larocca@mail.utoronto.ca>
2016-03-04 16:55:50 -0500

    PROJ3-3 Added a few comments for clarity

--- javafiles/Accounts.java
+++ javafiles/Accounts.java
@@ -3,19 +3,30 @@ package project;
 import java.sql.Date;
 
 public class Accounts {
+  /*
+   * Static variables to keep track of account types
+   */
+  
   static final int TYPE_STUDENT = 0;
   static final int TYPE_PROFESSOR = 1;
-  
+
+  /**
+   * All variables needed for an account
+   */
+
   int accountType;
   String firstName;
   String lastName;
   String email;
   Date birthday;
-  
-  public Accounts(){
-    
+
+  public Accounts() {
+
   }
-  
+
+  /*
+   * Start of Getters and Setters, more functions to be added
+   */
   public int getAccountType() {
     return accountType;
   }
