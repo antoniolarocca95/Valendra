f56ac4014f985a559c82c1d5cbe4e651dc75be18
Kelvin Kong <kelvink.kong@mail.utoronto.ca>
2016-03-04 22:05:00 -0500

    PROJ3-1 adding Parsing Skeleton code

---
+++ javafiles/Parser.java
@@ -0,0 +1,16 @@
+public class Parser{
+	private String name;
+	private Int docNumber;
+
+	public Parser{
+
+	}
+	//Gets Parsed Document
+	public getParsedDoc{
+
+	}
+	//Sets Parsed Document
+	public setParsedDoc{
+
+	}
+}
