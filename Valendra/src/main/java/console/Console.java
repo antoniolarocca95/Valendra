cf4750daceeb9ab50b2fd562c5797047079bafa7
Antonio La Rocca <antonio.larocca@mail.utoronto.ca>
2016-03-11 14:12:10 -0500

    PROJ3-32 Creating a method to register

---
+++ Valendra/src/main/java/console/Console.java
@@ -0,0 +1,10 @@
+package console;
+
+public class Console {
+	public static void main(String[] args){
+	}
+	
+	private void register(String fname, String lname, String username, String password, String email){	
+		
+	}
+}
