3d57779afbe7bd2fda0c368eda943a475494dacc
laroccaa <laroccaa@dh2026pc01.erin.utoronto.ca>
2016-03-17 16:09:03 -0400

    PROJ3-42 Implemented a basic PDF parser

--- Valendra/src/main/java/parser/PDFParser.java
+++ Valendra/src/main/java/parser/PDFParser.java
@@ -1,18 +1,21 @@
 package parser;
 
 public class PDFParser extends Parser{
-	private String name;
-	private int docNumber;
-
-	public PDFParser(){
-
-	}
-	//Gets Parsed Document
-	public void getParsedDoc(){
-
-	}
-	//Sets Parsed Document
-	public void setParsedDoc(){
-
+	public static String parse(String path){
+		PDFTextStripper pdfStripper = null;
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+		File file = new File(path);
+		
+		try {
+			PDFParser parser = new PDFParser(new FileInputStream(file));
+			parser.parse();
+			pdfStripper = new PDFTextStripper();
+			pdDoc = new PDDocument(cosDoc);
+			String parsedText = pdfStripper.getText(pdDoc);
+			return parsedText;
+		} catch (IOException e) {
+			return '';
+		}
 	}
 }
