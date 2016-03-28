0344a9beab44c6e97117296702505963f504c145
Antonio La Rocca <antonio.larocca@mail.utoronto.ca>
2016-03-28 16:30:35 -0400

    Adding the current style sheets and javascript

--- HTML/301proj/error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	echo '<style> body{background: url(odd_wallpaper.jpg);} form {text-align: center; display:inline-block;} h1{color:#ffffcc;}</style>';
-	session_save_path('/student/aksoyert/www/csc301Proj/sessions');
-	session_start();
-
-	echo "error";
-
-?>
